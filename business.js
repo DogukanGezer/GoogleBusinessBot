bda43d4b945d34d526cb0821c1d50a9d9005677b
Dogukan <doukan_gezer@hotmail.com>
2022-05-15 00:25:51 +0300

    delete getJson method

--- business.js
+++ business.js
@@ -8,17 +8,6 @@ class business {
         this.phone = phone
         this.time = time
     }
-
-    getJson() {
-        const json = {
-            title: this.title,
-            adress: this.adress,
-            page: this.webPage,
-            phone: this.phone,
-            time: this.time
-        }
-        return json
-    }
 }
 
 
